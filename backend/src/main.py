017b89a42345b7d2af6ea3d548893edc569018c0
Sun <leo.sun@ubrigene.com>
2024-01-20 15:51:42 -0800

    fix merge conflict2

--- backend/src/main.py
+++ backend/src/main.py
@@ -17,17 +17,3 @@ t.run()
 
 while (True):
     print(bot.States[-1].Result)
-
-# Test PDFExtractor
-# Load a PDF file into a buffer (replace 'your_pdf_file.pdf' with your actual file)
-with open('tests\sample_resume.pdf', 'rb') as file:
-    pdf_buffer = file.read()
-
-# Create an instance of PDFExtractor
-pdf_extractor = PDFExtractor()
-
-# Extract text from the PDF buffer
-extracted_text = pdf_extractor.extract_text_from_pdf(pdf_buffer)
-
-# Print the extracted text
-print(extracted_text)
