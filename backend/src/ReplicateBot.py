09a39afdf63534788f2640c3891418c153c7dae5
rishit-singh <rsrishitsingh@gmail.com>
2024-01-20 12:09:42 -0800

    Created a Next project

--- backend/src/ReplicateBot.py
+++ backend/src/ReplicateBot.py
@@ -1,6 +1,7 @@
 import replicate
+import requests
 from typing import Any
-
+import asyncio
 class Queue:
     def __init__(self, initList: list[Any] = []):
         self.Buffer: list[Any] = initList
@@ -19,26 +20,41 @@ class Queue:
 
             return item
 
+PromptFormats: dict[str, str] = {
+    "user": """[INST]{}[/INST]""",
+    "assistant": """{}"""
+}
+
 class Message:
     def __init__(self, role: str, content: str):
         self.Role: str = role
         self.Content: str = content
 
     def __str__(self) -> str:
-        return str()
+        return PromptFormats[self.Role].format(self.Content)
 
 class ReplicateBot:
     def __init__(self, model: str, apiKey: str):
-        self.Model: str = model
+        self.Model = replicate.models.get(model)
+        self.ModelVersion = self.Model.versions.list()[-1]
         self.APIKey: str = apiKey
         self.MessageQueue: Queue = Queue()
         self.Results: list = []
+        self.PromptStr: str = ""
+        self.PromptCount: int = 0
 
     def Prompt(self, message: Message):
         self.MessageQueue.Enqueue(message)
 
+    def FetchTokens(self, url: str):
+
+        pass   
+
     def Run(self):
         while (not(self.MessageQueue.IsEmpty())):
-            self.Results.append(replicate.run(self.Model, input= {
-                "prompt": self.MessageQueue.Dequeue() 
-            }))
+            self.PromptStr += str(self.MessageQueue.Dequeue())
+            result = replicate.predictions.create(version=self.ModelVersion, input = {
+                "prompt": self.PromptStr
+            }, stream=True)
+
+        asyncio.create_task(self.FetchTokens(self, result.urls["stream"])) 
--- backend/src/main.py
+++ backend/src/main.py
@@ -4,9 +4,7 @@ from ReplicateBot import ReplicateBot
 
 bot = ReplicateBot("mistralai/mixtral-8x7b-instruct-v0.1", os.getenv("REPLICATEKEY"))
 
-bot.Prompt("Hello Bot.")
+bot.Prompt(sys.argv[1])
 bot.Run()
 
 print(bot.Results)
-
- 
