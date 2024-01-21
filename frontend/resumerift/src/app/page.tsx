ded5e27b4bcd4b540e47b931f728729030d38b11
rishit-singh <rsrishitsingh@gmail.com>
2024-01-20 21:25:38 -0800

    Implemented uploads

--- frontend/resumerift/src/app/page.tsx
+++ frontend/resumerift/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "./Header";
 import { Button, Tab, Tabs } from "@material-ui/core";
 import Conditional from "../../Conditional";
@@ -13,7 +13,7 @@ function tabProps(index: number) {
       'aria-controls': `simple-tabpanel-${index}`,
     };
 }
-  
+
 export default function Index()
 {
     const [tabView, setTabView] = useState(1);
@@ -24,6 +24,20 @@ export default function Index()
     const md =`# Your Name\n## Contact Information\n- **Email:** your.email@example.com\n- **Phone:** (555) 123-4567\n- **LinkedIn:** [LinkedIn Profile](https://www.linkedin.com/in/yourname)\n- **GitHub:** [GitHub Profile](https://github.com/yourusername)
                 `; 
 
+
+    const uploadFile =  async () => {
+            const formData: FormData = new FormData();
+
+            formData.append("uploaded_file", (pdfRef.current?.files as FileList)[0]);
+            const response = await fetch(`http://${process.env.APIURL}/upload/resume?id=ec9b549f-b24f-4c1a-bdc2-244deee34364`, {
+                method: "POST",
+                mode: "cors",
+                body: formData 
+            });          
+
+            console.log((await response.json())); 
+        };
+    
     return (
         <div className="grid grid-rows-[8vh_92vh]">
             <Header/>
@@ -38,6 +52,7 @@ export default function Index()
                         <Conditional Condition={() => tabView == 0}>
                             <PDFViewer Path={path}/>
                             <input type="file" ref={pdfRef} onChange={() => setPath((pdfRef.current?.files as FileList)[0].name)}></input>
+                            <Button variant="contained" onClick={async () => await uploadFile()}>Upload</Button>
                         </Conditional>
 
                         <Conditional Condition={() => tabView == 1}>
