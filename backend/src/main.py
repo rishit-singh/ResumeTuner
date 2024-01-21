93b9d7911983f8b2cbe703c8695aad89acd17827
Anderson T <tseng.andersonn@gmail.com>
2024-01-20 16:48:31 -0800

    start api

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,21 +2,24 @@ import os
 import sys
 import json
 import threading
+import uvicorn
+
 from dotenv import load_dotenv
 load_dotenv()
-from ReplicateBot import ReplicateBot, Message
 
-os.environ["REPLICATE_API_TOKEN"] = os.getenv("REPLICATEKEY")
+# from ReplicateBot import ReplicateBot, Message
 
-bot = ReplicateBot("mistralai/mixtral-8x7b-instruct-v0.1", os.getenv("REPLICATEKEY"))
+# bot = ReplicateBot("mistralai/mixtral-8x7b-instruct-v0.1", os.getenv("REPLICATE_API_TOKEN"))
 
-bot.Prompt(Message("user", sys.argv[1]))
+# bot.Prompt(Message("user", sys.argv[1]))
 
-def Main():
-    bot.Run()
+# def Main():
+#     bot.Run()
 
-t = threading.Thread(target=Main)
-t.run()
+# t = threading.Thread(target=Main)
+# t.run()
 
-while (True):
-    print(bot.States[-1].Result)
+# while (True):
+#     print(bot.States[-1].Result)
+
+uvicorn.run("python.api:app", host="0.0.0.0", port=5000)
